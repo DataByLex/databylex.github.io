c4a16afa5821d9e01df117ed599628c5d3ab8ad3
Kavoon <kavoon.dev@gmail.com>
2025-04-13 16:56:17 +0300

    Setup site for databylex.github.io

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./**/*.html", "./**/*.md"],
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ['Poppins', 'sans-serif'],
+                sans: ['Open Sans', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
